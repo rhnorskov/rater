68c6c76bec2b4c0546fc6430cb8482e738735e7e
Rasmus Nørskov <rasmus@norskov.org>
2024-11-07 11:45:46 +0100

    feat: add movies (#1)

--- src/app/movie/page.tsx
+++ src/app/movie/page.tsx
@@ -7,50 +7,44 @@ import { getPosterUrl } from "@/services/tmdb/poster";
 import { MovieHeader } from "./header";
 
 export default async function MoviePage() {
+  const [movieA, movieB] = await findRandom(2);
+
+  if (!movieA || !movieB) {
+    return notFound();
+  }
+
   return (
-    <div>
-      <h1>Movie Page</h1>
-    </div>
+    <>
+      <MovieHeader />
+      <div className="flex h-full">
+        <button className="group flex h-full w-1/2 flex-col items-end justify-center gap-4 pr-8">
+          <div className="relative aspect-[2/3] w-64 border-8 border-transparent group-hover:border-gray-700">
+            <Image
+              fill
+              src={getPosterUrl(movieA.posterPath, "w342")}
+              alt={movieA.title}
+            />
+          </div>
+
+          <div className="h-16 w-64">
+            {movieA.title} ({movieA.releaseDate.getFullYear()})
+          </div>
+        </button>
+
+        <button className="group flex h-full w-1/2 flex-col items-start justify-center gap-4 pl-8">
+          <div className="relative aspect-[2/3] w-64 border-8 border-transparent group-hover:border-gray-700">
+            <Image
+              fill
+              src={getPosterUrl(movieB.posterPath, "w342")}
+              alt={movieB.title}
+            />
+          </div>
+
+          <div className="h-16 w-64">
+            {movieB.title} ({movieB.releaseDate.getFullYear()})
+          </div>
+        </button>
+      </div>
+    </>
   );
-
-  // const [movieA, movieB] = await findRandom(2);
-
-  // if (!movieA || !movieB) {
-  //   return notFound();
-  // }
-
-  // return (
-  //   <>
-  //     <MovieHeader />
-  //     <div className="flex h-full">
-  //       <button className="group flex h-full w-1/2 flex-col items-end justify-center gap-4 pr-8">
-  //         <div className="relative aspect-[2/3] w-64 border-8 border-transparent group-hover:border-gray-700">
-  //           <Image
-  //             fill
-  //             src={getPosterUrl(movieA.posterPath, "w342")}
-  //             alt={movieA.title}
-  //           />
-  //         </div>
-
-  //         <div className="h-16 w-64">
-  //           {movieA.title} ({movieA.releaseDate.getFullYear()})
-  //         </div>
-  //       </button>
-
-  //       <button className="group flex h-full w-1/2 flex-col items-start justify-center gap-4 pl-8">
-  //         <div className="relative aspect-[2/3] w-64 border-8 border-transparent group-hover:border-gray-700">
-  //           <Image
-  //             fill
-  //             src={getPosterUrl(movieB.posterPath, "w342")}
-  //             alt={movieB.title}
-  //           />
-  //         </div>
-
-  //         <div className="h-16 w-64">
-  //           {movieB.title} ({movieB.releaseDate.getFullYear()})
-  //         </div>
-  //       </button>
-  //     </div>
-  //   </>
-  // );
 }
